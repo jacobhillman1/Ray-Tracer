c674d128504fc6281f613a10b3d379390fa9e6ff
Jacob Hillman <jacobhillman@Jacobs-MacBook-Pro-2.local>
2017-10-23 21:38:52 -0400

    lightbox updated to remove commented blocks

--- src/lightbox.cpp
+++ src/lightbox.cpp
@@ -15,25 +15,33 @@ Vec3d calculate_surface_normal_on_sphere(Vec3d intersection, Ray<double, 3> r, d
 Vec3d find_sphere_intersections(Ray<double, 3> r, double radius, Vec3d center);
 
 
-// TODO: template a "shape" class
+// Trace function accepts two spheres which will be represented in the output image.
+// Function returns the color that a pixel should be after the ray fired through 
+// it interacts with the spheres. 
 Vec3d traceWithObjects(Ray<double, 3> r, Sphere s1, Sphere s2, int depth) {
     RayHit hitObject = RayHit(r);
     s1.checkHit(hitObject);
     if(!hitObject.hitSomething()) {
         s2.checkHit(hitObject);
     }
+
     if(hitObject.hitSomething()) {
-         return hitObject.getAlbedo() * hitObject.getDotProduct()
+        hitObject.generateNewDirection();
+        return hitObject.getAlbedo() * hitObject.getDotProduct()
                 * traceWithObjects(Ray<double, 3>(hitObject.getHitPoint(), hitObject.getNewDirection()), s1, s2, depth + 1);
     }
     else {
+        // the color that the "sky" will be
         return Vec3d(1, 1, 1);
     }
 }
 
+// Most of the image rendering was provided with the boilerplate
 void render_image(Image<double,3>& img) {
-    Sphere s1  = Sphere(Vec3d(0.0, 0.0, -4.0), 0.5, Vec3d(0.0, 0.0, 1.0));
-    Sphere s2 = Sphere(Vec3d(1.0, 0.0, -4.0), 0.3, Vec3d(1.0, 0.0, 0.0));
+	// Instantiate the sphere objects
+    Sphere s1  = Sphere(Vec3d(0, 0.0, -4.0), 0.5, Vec3d(1, 1, 1));
+    Sphere s2 = Sphere(Vec3d(0, -40.0, -4.0), 39.3, Vec3d(1, 0.0, 0.0));
+    int numRays = 50; // number of rays fired through each pixel
     for (index_t y = 0; y < img.height; ++y) {
         for (index_t x = 0; x < img.width; ++x) {
             Vec2i xy = Vec2i(x,y);                // integer pixel coordinates
@@ -46,11 +54,10 @@ void render_image(Image<double,3>& img) {
 
             Vec3d color = Vec3d(0.0, 0.0, 0.0);
 
-            for (int i = 0; i < 50; i++) {
+            for (int i = 0; i < numRays; i++) {
                 color += traceWithObjects(r, s1, s2, 0);
             }
-
-            color /= 50;
+            color /= numRays;
 
             img[xy] = color;
         }
@@ -66,24 +73,3 @@ int main(int argc, char** argv) {
 
     return 0;
 }
-
-/*
- * TODO: make a sphere object that takes in the center and radius
- * you can pass that sphere object a rayhit object
- * if it doesn't hit the sphere, the rayhit object has a bool value that you
- * set to false. if it does it, you store the location of the hit, the normal,
- * and generate a new random direction.
-trace {
-    for(int i = 0; i < 10; i++) {
-        Sphere s = Sphere(radius, center);
-        s.checkHit(rayHitObj);
-        if rayHitObj.hit() {
-            trace(rayHitObj);
-            return colorFormula;
-        }
-        else {
-            return skyColor
-        }
-    }
-}
-*/
